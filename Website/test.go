635854d2dcf89dc4d81bd227e7854d3783584a50
Clément Teyssandier <clement.teyssandier@supinfo.com>
2021-02-04 12:21:00 +0100

    Test connect get send

---
+++ Website/test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"fmt"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const senderAdress = "0xf54af0421232CdE80124125c23D0c7DF6933Ea18"
+const receiverAdress = "0xbD89dFEBE2194a2c5a6456E238d713751187CedC"
+const senderPrivate = ""
+
+func main() {
+	client := connect("https://rinkeby.infura.io/v3/67bba885733241c09e98ccb5e630bd2c")
+	getWallet(senderAdress, client)
+	send(senderAdress, receiverAdress, client)
+	getWallet(senderAdress, client)
+}
+
+func send(sender string, receiver string, client *ethclient.Client) {
+	privateKey, err := crypto.HexToECDSA(senderPrivate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	value := big.NewInt(0100000000000000000) // in wei (1 eth)
+	gasLimit := uint64(21000)                // in units
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toAddress := common.HexToAddress(receiver)
+	var data []byte
+	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+}
+
+func connect(url string) *ethclient.Client {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("we have a connection")
+	return client
+}
+
+func getWallet(sender string, client *ethclient.Client) {
+	account := common.HexToAddress(senderAdress)
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25893180161173005034
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
+}
