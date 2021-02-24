d594020af7b6fa4fbc32be9f96bd1f58e5ec6011
Clément Teyssandier <clement.teyssandier@supinfo.com>
2021-02-24 17:49:58 +0100

    test use metamask in js

--- Website/router/router.go
+++ Website/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/ClementTeyssa/5BLOCK/Website/controllers"
 	"github.com/gorilla/mux"
 )
@@ -10,6 +12,8 @@ func InitializeRouter() *mux.Router {
 	// StrictSlash is true => redirect /users/ to /users
 	router := mux.NewRouter().StrictSlash(true)
 	router.Methods("GET").Path("/").Name("Index").HandlerFunc(controllers.Index)
+	router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	// router.Methods("GET").Path("/users").Name("Index").HandlerFunc(controllers.UsersIndex)
 	// router.Methods("POST").Path("/users").Name("Create").HandlerFunc(controllers.UsersCreate)
 	// router.Methods("GET").Path("/users/{id}").Name("Show").HandlerFunc(controllers.UsersShow)
