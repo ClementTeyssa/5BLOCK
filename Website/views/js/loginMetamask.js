2a67724613198e9b0f36d8d69cf113b2bc82717c
Clément Teyssandier <clement.teyssandier@supinfo.com>
2021-03-01 15:46:50 +0100

    for merge

--- Website/views/js/loginMetamask.js
+++ Website/views/js/loginMetamask.js
@@ -1,28 +1,32 @@
-if (typeof window.ethereum !== 'undefined') {
-    //login
-    console.log('MetaMask is installed!');
-    const ethereumButton = document.querySelector('.enableEthereumButton');
-    if(!ethereum.isConnected()) {
-        ethereumButton.addEventListener('click', () => {
-            ethereum.request({ method: 'eth_requestAccounts' });
+function initMetamask() {
+    if (typeof window.ethereum !== 'undefined') {
+        //login
+        console.log('MetaMask is installed!');
+        const ethereumButton = document.querySelector('.enableEthereumButton');
+        if(!ethereum.isConnected()) {
+            console.log('MetaMask is not connected!');
+            ethereumButton.addEventListener('click', () => {
+                ethereum.request({ method: 'eth_requestAccounts' });
+            });
+        } else {
+            console.log('MetaMask is connected!');
+            // document.querySelector('.enableEthereumButton').remove();
+        }
+    
+        //print ETH Adress
+        const ethereumButtonShow = document.querySelector('.showEthereumButton');
+        const showAccount = document.querySelector('.showAccount');
+    
+        ethereumButtonShow.addEventListener('click', () => {
+          getAccount();
         });
+    
+        async function getAccount() {
+          const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+          const account = accounts[0];
+          showAccount.innerHTML = account;
+        }
     } else {
-        document.getElementsByClassName("enableEthereumButton").remove();
+        alert('MetaMask is not installed!');
     }
-
-    //print ETH Adress
-    const ethereumButtonShow = document.querySelector('.showEthereumButton');
-    const showAccount = document.querySelector('.showAccount');
-
-    ethereumButtonShow.addEventListener('click', () => {
-      getAccount();
-    });
-
-    async function getAccount() {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      const account = accounts[0];
-      showAccount.innerHTML = account;
-    }
-} else {
-    alert('MetaMask is not installed!');
 }
